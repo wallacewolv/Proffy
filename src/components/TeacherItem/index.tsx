41f75e5732ac9a646b56f331cf758b4511b5c394
unknown <wallace_wolv@hotmail.com>
2020-08-08 01:48:10 -0300

    Adicionado Pasta TeacherItem

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import whatsappIcon from'../../assets/images/icons/whatsapp.svg';
+
+import './styles.css'
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src="https://avatars1.githubusercontent.com/u/65473975?s=460&u=77431e03e161ea43a2b9b689c61b5cad0d85282c&v=4" alt="Wallace Wesley" />
+        <div>
+          <strong>Wallace Wesley</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
-import whatsappIcon from'../../assets/images/icons/whatsapp.svg';
+import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css'
 
@@ -29,32 +29,11 @@ function TeacherList() {
       </PageHeader>
 
       <main>
-        <article className="teacher-item">
-          <header>
-            <img src="https://avatars1.githubusercontent.com/u/65473975?s=460&u=77431e03e161ea43a2b9b689c61b5cad0d85282c&v=4" alt="Wallace Wesley"/>
-            <div>
-              <strong>Wallace Wesley</strong>
-              <span>Química</span>
-            </div>
-          </header>
-
-          <p>
-          Entusiasta das melhores tecnologias de química avançada.
-          <br/>
-          Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-          </p>
-
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$ 80,00</strong>
-            </p>
-            <button type="button">
-              <img src={whatsappIcon} alt="Whatsapp"/>
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
       </main>
     </div>
   );
