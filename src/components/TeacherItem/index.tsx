56de9f7d895eab13c278c238efbd8b69e4d8fcf4
unknown <wallace_wolv@hotmail.com>
2020-08-19 20:13:40 -0300

    Adicionando mobile

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,34 +2,56 @@ import React from 'react';
 
 import whatsappIcon from'../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css'
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string; 
+  subject: string; 
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  };
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars1.githubusercontent.com/u/65473975?s=460&u=77431e03e161ea43a2b9b689c61b5cad0d85282c&v=4" alt="Wallace Wesley" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Wallace Wesley</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br />
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          target='blank' 
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
